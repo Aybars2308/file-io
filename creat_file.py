a2f4274e0b74bb18b292bd2c3bdc2d7d304f882c
Aybars2308 <aybarserdogan2308@gmail.com>
2024-05-06 17:52:24 +0300

    creat file

---
+++ creat_file.py
@@ -0,0 +1,9 @@
+# f = open("dosya_adi", "kip")
+# kip = w = yazma kipi(write)
+# kip = r = okuma kipi(read)
+# kip = a = hepsi (all)
+#***********
+# file = open(dosya_adi)
+# file.read(), file.write()
+
+kayit_dosyasi = open("kayit_dosyasi.txt", "w")
